74e8be98f61e1a6006dea43a4cab3f991c4a08dc
Jason Anderson <jasonanderson@uchicago.edu>
2022-02-25 16:06:13 -0600

    clean up peer deletion

--- tunelo/api/channels.py
+++ tunelo/api/channels.py
@@ -211,7 +211,7 @@ def destroy_channel(uuid):
         uuid: the UUID of the channel must be equivalent to the ``id`` field
         of a spoke port.
     """
-    spoke, peers = get_channel_by_uuid(uuid)
+    _, peers = get_channel_by_uuid(uuid)
     neutron = get_neutron_client()
 
     try:
@@ -219,13 +219,13 @@ def destroy_channel(uuid):
     except PortNotFoundClient:
         raise NotFound(f"Channel {uuid} not found.")
 
-    # After deleting the spoke, we have to remove the spoke from its peer hub(s)
-    for hub in peers:
-        hub_peers = hub["binding:profile"]["peers"]
-        hub_id = get_channel_uuid(hub)
-        # If the hub has no peers left, it should be deleted
-        if not hub_peers:
-            neutron.delete_port(hub_id)
+    for peer in peers:
+        peer_peers = peer["binding:profile"]["peers"]
+        # If removing the last peer from our peer, our peer is now an orphan and can
+        # also be cleaned up.
+        # TODO(jason): this should probably be optional behavior in the future.
+        if len(peer_peers) == 1:
+            neutron.delete_port(get_channel_uuid(peer))
 
 
 @route(
