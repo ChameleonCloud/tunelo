ba9e600b830cb8d8b1aa7238455111f3f26daf20
Michael Sherman <shermanm@uchicago.edu>
2026-05-22 11:37:48 -0500

    Fix build and deps for 2023.1
    Fixes build of sample config file, removes unused
    imports, and removes importlib checks that were needed
    for python <= 3.7.

--- tunelo/__init__.py
+++ tunelo/__init__.py
@@ -11,12 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-try:
-    from importlib.metadata import version
-except Exception:
-    # Python < 3.7
-    from importlib_metadata import version
-
+from importlib.metadata import version
 PROJECT_NAME = "tunelo"
 
 try:
